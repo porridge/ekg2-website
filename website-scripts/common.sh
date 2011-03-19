737f6125063775cc15009c161686a512c20d6918
Marcin Owsiany <marcin@owsiany.pl>
2011-03-19 13:16:59 +0000

    Another approach to logging.

--- website-scripts/common.sh
+++ website-scripts/common.sh
@@ -3,14 +3,25 @@
 set -e
 
 website=$HOME/ekg2
-logs=$site/script-logs
+logs=$website/script-logs
 
-function logged_command()
+function logging_prepare()
 {
 	log="$1"; shift
 	mkdir -m 0755 -p $logs
 	savelog -m 0644 -qtl "${logs}/${log}"
-	( "$@" ) > "${logs}/${log}" 2>&1
 }
 
+function logged_command()
+{
+	log="$1"; shift
+	logging_prepare "$log"
+	"$@" > "${logs}/${log}" 2>&1
+}
+
+function logging_start()
+{
+	log="$1"; shift
+	logged_command "$log" exec
+}
 
